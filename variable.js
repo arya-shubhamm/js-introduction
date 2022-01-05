ed3b3a9403910048fd10406818841792732c4163
shubhamarya214@gmail.com <shubhamarya214@gmail.com>
2022-01-05 23:34:35 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,78 @@
+// variable declaration
+
+
+// var a=2;
+// console.log(a)
+// var a='i am string'
+// console.log(a)
+// var a= true
+// console.log(a)
+
+/*let a=3
+console.log(a)
+let a= 'hello'
+console.log(a) // redeclaration not allowed in let
+*/
+
+// let a='bye'
+// console.log(a)
+// a='hello'
+// console.log(a) // value can reasign in  let
+
+
+//loops in JS
+//for prime number
+// var flag = true
+// var num = 16
+// for (var i = 2; i * i <= num; i++) {
+//     if (num % i == 0) {
+//         flag = false
+//         break
+//     }
+// }
+
+// if (flag) {
+//     console.log('number is prime')
+// }
+// else {
+//     console.log('number is not prime')
+// }
+
+//FUNCTIONS
+
+// function multiply(a,b){
+//     console.log('Multiplication is', a*b)
+// }
+// multiply(2,3)
+
+
+// ARRAY
+
+// let arr=[]
+// console.log(arr)
+
+ let cars= ['bmw', 'jaguar', 'tesla','ferrari']
+// console.log(cars[2])
+// console.log(cars[1])
+// console.log(cars[0])
+// console.log(cars[3])
+
+//METHOD
+
+console.log(cars.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
